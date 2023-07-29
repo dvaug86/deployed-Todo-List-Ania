b6be8ce725bac5046bfbb623497d07c0ead6efe1
dv <dvaug86@gmail.com>
2023-07-29 18:22:05 -0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import ListHeader from "./components/ListHeader";
+import ListItem from "./components/ListItem";
+import Auth from "./components/Auth";
+
+const App = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const userEmail = cookies.Email;
+  const authToken = cookies.AuthToken;
+  const [tasks, setTasks] = useState(null);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`
+      );
+
+      const json = await response.json();
+      setTasks(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (authToken) {
+      getData();
+    }
+  }, []);
+
+  console.log(tasks);
+
+  //sort by date
+  const sortedTasks = tasks?.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
+  return (
+    <div className="app">
+      {/* no Auth token (!authToken) it'll show the auth modal aka popup */}
+      {!authToken && <Auth />}
+
+      {authToken && (
+        <>
+          <ListHeader listName={"Holiday check list"} getData={getData} />
+          <p className="user-email">Welcome back! {userEmail}</p>
+          {sortedTasks?.map((task) => (
+            <ListItem key={task.id} task={task} getData={getData} />
+          ))}
+        </>
+      )}
+      <p className="copyright">Creative Coding LLC</p>
+    </div>
+  );
+};
+
+export default App;
